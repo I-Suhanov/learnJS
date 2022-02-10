6d702e5689d427fc988da5625af121800a3f873a
Suhanov <ivan.suhanov5@yandex.ru>
2022-02-10 14:00:07 +0300

    learn functions

---
+++ js/function.js
@@ -0,0 +1,34 @@
+"use strict";
+
+let num = 20;
+
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 26;
+    console.log(num);
+}
+
+showFirstMessage("Hello world");
+console.log(num); 
+
+function calc(a, b) {
+    return (a + b);
+}
+
+console.log(calc(5, 7));
+console.log(calc(7, 2));
+console.log(calc(9, 1));
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function() {
+    console.log("Hello");
+};
+
+logger();
