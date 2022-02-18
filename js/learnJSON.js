6d01fe947ad09fe135ec6b2adea1a03364cb7a4c
Suhanov <ivan.suhanov5@yandex.ru>
2022-02-18 15:33:11 +0300

    learn promice

--- js/learnJSON.js
+++ js/learnJSON.js
@@ -3,6 +3,14 @@
 const persone = {
     name: 'Alex',
     tel: '+78954564504',
+    parents: {
+        mom: 'Jes',
+        dad: 'Pit'
+    }
 };
 
+const clone = JSON.parse(JSON.stringify(persone));
+clone.parents.mom = 'Ann';
+console.log(clone);
+console.log(persone);
 console.log(JSON.stringify(persone));
